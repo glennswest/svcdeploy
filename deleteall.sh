75aeb2d93126a7d300906b86d8d5f51128b524d2
Glenn West <glennswest@neuralcloudcomputing.com>
2017-03-19 19:56:08 +0800

    cleanup messages

---
+++ deleteall.sh
@@ -0,0 +1,4 @@
+oc delete is/svcperson
+oc delete dc/svcperson
+oc delete po/svcperson-deploy
+oc delete rc/svcperson-1
--- deploy.js
+++ deploy.js
@@ -1,121 +1,88 @@
-var mdns = require('mdns-js');
 var util = require('util');
-var process = require('process');
-var fssync = require('fs-sync');
-var fs = require('fs');
 
-var execFile=require('child_process').execFile;
-myIP = process.env.myIP;
-console.log("My IP is: " + myIP);
+var exec=require('child_process').exec;
 
-servers = [];
+var svc_person = {name:'svcperson', image:'glennswest/rockbase', port:"8080", stateful:true,mnt:'/data'};
+var svc_mqtt   = {name:'svcmqtt',   image:'glennswest/svcmqtt',  port:"8080", stateful:true,mnt:'/data'};
 
-var ad = mdns.createAdvertisement(mdns.tcp('mqtt'), 1883,{
-          name:'svcmqtt',
-          txt:{
-            txtvers:'1'
+//var deploy_list = [svc_person,
+//                   svc_mqtt];
+var deploy_list = [svc_person];
+
+workq = [];
+
+function dosystem(thecmdline)
+{
+	thevalue = {};
+        thevalue.cmd = thecmdline;
+        workq.push(thevalue);
+        if (workq.length == 1){
+           dosystemexec();
            }
-         });
-ad.start();
+}
 
-
-// Start up server
-function restart_server(data){
-
-        if (!data){
-           console.log("Starting Mosquitto");
-          } else {
-           console.log("Mosquitto - " + data + "Restarting");
-          }
-        child = execFile('/usr/sbin/mosquitto',['-v','-c','/etc/mosquitto/mosquitto.conf'], {cwd: "/etc/mosquitto"}, (error, stdout, stderr) => {
+function dosystemexec()
+{
+        
+        if (workq.length == 0){
+           return;
+           }
+        thevalue = workq[0];
+        thecmdline = thevalue.cmd;
+        child = exec(thecmdline,(error, stdout, stderr) => {
                 if (error){
-                   console.log(stderr);
                    console.log("Error On Exec: " + util.inspect(error) + "");
                    }
-                console.log(stdout);
-                //restart_server("Restarting Mosquitto");
                 });
-        console.log("Server PID= " + util.inspect(child.pid) + "");
+        child.on('close',function(data){
+               workq.shift(); // Remove currently operating entry
+               dosystemexec();
+               });
         child.stderr.on('data',function(data){
             console.log(data.toString());
             });
         child.stdout.on('data',function(data){
-            console.log(data.toString());
+            thevalue = data.toString();
+            console.log(thevalue);
             });
-        server_pid = child.pid;
 }
 
-// mosquitto-base.conf
-function update_config()
+function deployvolume(name,mntpoint)
 {
-	theline = "address ";
-        servers.forEach(function(element){
-              theline = theline + element.ip + ":1883  ";
-              });
-	options = {};
-        options.force = true;
-        fssync.copy('/etc/mosquitto/mosquitto-base.conf','/etc/mosquitto/mosquitto.conf',options);
-        fs.appendFileSync('/etc/mosquitto/mosquitto.conf','\n# Updated by svcmqtt\n');
-        if (servers.length == 0) return; // Dont do empty connections
-        fs.appendFileSync('/etc/mosquitto/mosquitto.conf','connection sitecom\n');
-        fs.appendFileSync('/etc/mosquitto/mosquitto.conf',theline);
-        fs.appendFileSync('/etc/mosquitto/mosquitto.conf',"start_type automatic");
+// oc volume dc/svcperson --add --mount-path=/data --type=persistentVolumeClaim --claim-name=svcperson
+
+         dosystem('oc volume dc/' + name + ' --add  --mount-path=' + mntpoint + ' --type=persistentVolumeClaim ' + '--claim-name=' + name +
+                  ' --claim-mode="ReadWriteOnce" --claim-size=10G');
+
+
 }
 
-
-
-// watch all mqtt servers 
-var browser = mdns.createBrowser(mdns.tcp('mqtt'));
-browser.on('ready', function onReady(){
-      console.log('browser is ready');
-      setTimeout(restart_server,3000);
-      browser.discover();
-      });
-//service up:  { addresses: [ '192.168.1.231' ],
-//  query: [],
-//  port: 1883,
-//  fullname: 'svcmqtt._mqtt._tcp.local',
-//  txt: [ 'txtvers=1' ],
-//  type: 
-//   [ { name: 'mqtt',
-//       protocol: 'tcp',
-//       subtypes: [],
-//       description: undefined },
-//     { name: 'mqtt',
-//       protocol: 'tcp',
-//       subtypes: [],
-//       description: undefined } ],
-//  host: 'svcmqtt.local',
-//  interfaceIndex: 2,
-//  networkInterface: 'pseudo multicast' }
-
-function findIP(ipaddr){
-     return servers.ip === ipaddr;
-     }
-function add_server(s)
+function deploycontainer(name,image)
 {
-   update_config();
-   if (s.ip == myIP){
-      console.log("Ignoring: Self");
-      return;
-      }
-   x = servers.find(o => o.ip === s.ip);
-   if (!x){
-       servers.push(server);
-       console.log(util.inspect(s));
-       console.log("Added " + s.ip);
-       }
+
+               dosystem('oc new-app ' + image + ' --name ' + name);
 }
-browser.on('update', function(service) {
-  server = {};
-  //console.log("service up: ", service);
-  ip = service.addresses[0];
-  service.type.forEach(function(element){
-       if (element.name == 'mqtt'){
-          server.ip = ip;
-          add_server(server);
-          }
-       });
-});
+
+function deployport(name,port)
+{
+               dosystem('oc expose dc/' + name + ' --port=' + port); 
+}
 
 
+function dodeploy(thevalue)
+{
+     console.log(util.inspect(thevalue));
+     console.log('Deploying ' + thevalue.name);
+     deploycontainer(thevalue.name,thevalue.image);
+     if (thevalue.stateful){
+        deployvolume(thevalue.name,thevalue.mnt);
+        }
+     deployport(thevalue.name,thevalue.port);
+}
+
+function deploy(thelist)
+{
+	thelist.forEach(dodeploy);
+} 
+
+deploy(deploy_list);
